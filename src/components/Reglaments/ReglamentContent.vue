0cf07a0516c8dcdea5394f2b5cbec65c8fb0fbe8
Egor Panteleev <jasonsuper04@gmail.com>
2022-10-08 13:58:57 +0300

    скелетон для регламентов

--- src/components/Reglaments/ReglamentContent.vue
+++ src/components/Reglaments/ReglamentContent.vue
@@ -47,125 +47,128 @@
         </ReglamentSmallButton>
       </router-link>
     </NavBar>
-    <div
-      v-if="!isTesting"
-      class="p-7 bg-white rounded-[28px] hide-toolbar"
-    >
-      <div class="flex flex-row justify-between leading-[30px] text-[13px] text-[#424242]">
-        <div class="flex justify-start">
-          <div
-            class="pr-2 border-r border-gray-200 overflow-y-auto scroll-style max-w-[200px] truncate"
-          >
-            <span class="font-medium pr-3">Отдел:</span>
-            <span class="text-[12px] ">{{ reglamentDep }}</span>
+    <ReglamentContentSkeleton v-if="reglamentSuccess === true" />
+    <div v-if="reglamentSuccess == false">
+      <div
+        v-if="!isTesting"
+        class="p-7 bg-white rounded-[28px] hide-toolbar"
+      >
+        <div class="flex flex-row justify-between leading-[30px] text-[13px] text-[#424242]">
+          <div class="flex justify-start">
+            <div
+              class="pr-2 border-r border-gray-200 overflow-y-auto scroll-style max-w-[200px] truncate"
+            >
+              <span class="font-medium pr-3">Отдел:</span>
+              <span class="text-[12px] ">{{ reglamentDep }}</span>
+            </div>
+            <div class="ml-2 flex">
+              <span class="font-medium pr-3">Автор:</span>
+              <EmployeeProfile
+                :photo="creatorFoto"
+                :name="creatorName"
+              />
+            </div>
           </div>
-          <div class="ml-2 flex">
-            <span class="font-medium pr-3">Автор:</span>
-            <EmployeeProfile
-              :photo="creatorFoto"
-              :name="creatorName"
-            />
+          <div>
+            <router-link :to="'/reglaments/' + $route.params.id + '/history'">
+              <a class="ml-2 hover:underline font-medium">
+                {{ lastCommentDate }}
+              </a>
+            </router-link>
           </div>
         </div>
-        <div>
-          <router-link :to="'/reglaments/' + $route.params.id + '/history'">
-            <a class="ml-2 hover:underline font-medium">
-              {{ lastCommentDate }}
-            </a>
-          </router-link>
-        </div>
-      </div>
-      <div
-        v-if="reglamentEditors.length"
-        class="flex justify-start leading-[30px] text-[13px] text-[#424242] flex-wrap"
-      >
-        <span class="font-medium pr-3">Редакторы:</span>
-        <EmployeeProfile
-          v-for="editor in reglamentEditors"
-          :key="editor"
-          class="mr-3"
-          :photo="editorFoto(editor)"
-          :name="editorName(editor)"
-        />
-      </div>
-      <QuillEditor
-        v-if="reglamentContent.length && !isTesting"
-        :key="reglamentContent"
-        v-model:content="reglamentContent"
-        content-type="html"
-        :read-only="true"
-        :toolbar="['']"
-        class="h-auto mx-auto mb-5 border-none 2xl:px-[145px] mt-[35px]"
-      />
-
-      <div
-        v-if="!isEditing && !isTesting && questions.length > 0 && !isContributor && shouldShowButton"
-        class="flex justify-center"
-      >
-        <button
-          class="flex font-medium items-end bg-[#FF912380] p-3 px-10 rounded-[8px] mt-2 text-[#2E2E2E] text-sm mr-1 hover:bg-[#F5DEB3]"
-          @click="startTheReglament"
+        <div
+          v-if="reglamentEditors.length"
+          class="flex justify-start leading-[30px] text-[13px] text-[#424242] flex-wrap"
         >
-          Пройти тест
-        </button>
-      </div>
-    </div>
-    <div
-      v-if="!isTesting && contributors.length"
-      class="mt-5 mb-5"
-    >
-      <button
-        class="flex items-center font-['Roboto'] text-[#424242] dark:bg-gray-700 dark:text-gray-100 rounded-lg text-[13px] font-medium hover:text-black transition"
-        @click="showEmployeesHandler"
-      >
-        <span class="mr-1.5">Сотрудники, прошедшие регламент</span>
-        <svg
-          width="12"
-          height="7"
-          viewBox="0 0 12 7"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M0.750134 0.493017C0.397701 0.850149 0.40151 1.42536 0.758642 1.7778L5.17757 6.13858C5.51147 6.46809 6.04134 6.48919 6.40039 6.18728L11.1876 2.16194C11.5716 1.83903 11.6211 1.26594 11.2982 0.881909C10.9753 0.497881 10.4022 0.448338 10.0182 0.771252L5.86461 4.26382L2.03491 0.484509C1.67778 0.132076 1.10257 0.135885 0.750134 0.493017Z"
-            :fill="showEmployees ? '#7E7E80' : '#4C4C4D'"
+          <span class="font-medium pr-3">Редакторы:</span>
+          <EmployeeProfile
+            v-for="editor in reglamentEditors"
+            :key="editor"
+            class="mr-3"
+            :photo="editorFoto(editor)"
+            :name="editorName(editor)"
           />
-        </svg>
-      </button>
+        </div>
+        <QuillEditor
+          v-if="reglamentContent.length && !isTesting"
+          :key="reglamentContent"
+          v-model:content="reglamentContent"
+          content-type="html"
+          :read-only="true"
+          :toolbar="['']"
+          class="h-auto mx-auto mb-5 border-none 2xl:px-[145px] mt-[35px]"
+        />
+
+        <div
+          v-if="!isEditing && !isTesting && questions.length > 0 && !isContributor && shouldShowButton"
+          class="flex justify-center"
+        >
+          <button
+            class="flex font-medium items-end bg-[#FF912380] p-3 px-10 rounded-[8px] mt-2 text-[#2E2E2E] text-sm mr-1 hover:bg-[#F5DEB3]"
+            @click="startTheReglament"
+          >
+            Пройти тест
+          </button>
+        </div>
+      </div>
       <div
-        v-if="showEmployees"
-        class="flex items-center mt-2.5 pb-2 flex-wrap gap-y-[10px]"
+        v-if="!isTesting && contributors.length"
+        class="mt-5 mb-5"
       >
         <button
-          v-if="canEdit"
-          class="flex items-center h-[30px] mr-3 px-[10px] py-[5px] bc-none border-none text-xs text-[#4C4C4D]"
-          @click="clearContributors"
+          class="flex items-center font-['Roboto'] text-[#424242] dark:bg-gray-700 dark:text-gray-100 rounded-lg text-[13px] font-medium hover:text-black transition"
+          @click="showEmployeesHandler"
         >
+          <span class="mr-1.5">Сотрудники, прошедшие регламент</span>
           <svg
-            class="mr-1.5"
-            width="20"
-            height="20"
-            viewBox="0 0 18 18"
+            width="12"
+            height="7"
+            viewBox="0 0 12 7"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              d="M3.37364 15.8768H17.7486V17.1268H3.37364V15.8768ZM16.1111 5.57051L11.1549 0.620514C11.0388 0.504294 10.9009 0.412095 10.7492 0.34919C10.5974 0.286284 10.4348 0.253906 10.2705 0.253906C10.1062 0.253906 9.94359 0.286284 9.79184 0.34919C9.64009 0.412095 9.50223 0.504294 9.38614 0.620514L0.636139 9.37051C0.519919 9.48661 0.42772 9.62447 0.364815 9.77621C0.30191 9.92796 0.269531 10.0906 0.269531 10.2549C0.269531 10.4192 0.30191 10.5818 0.364815 10.7336C0.42772 10.8853 0.519919 11.0232 0.636139 11.1393L3.45489 14.0018H9.44864L16.1111 7.33926C16.2274 7.22317 16.3196 7.08531 16.3825 6.93357C16.4454 6.78182 16.4777 6.61916 16.4777 6.45489C16.4777 6.29062 16.4454 6.12796 16.3825 5.97621C16.3196 5.82447 16.2274 5.68661 16.1111 5.57051ZM8.92989 12.7518H3.99864L1.49864 10.2518L5.44239 6.30801L10.3986 11.258L8.92989 12.7518ZM11.2799 10.4018L6.32989 5.44551L10.2486 1.50176L15.2486 6.45801L11.2799 10.4018Z"
-              fill="#4C4C4D"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M0.750134 0.493017C0.397701 0.850149 0.40151 1.42536 0.758642 1.7778L5.17757 6.13858C5.51147 6.46809 6.04134 6.48919 6.40039 6.18728L11.1876 2.16194C11.5716 1.83903 11.6211 1.26594 11.2982 0.881909C10.9753 0.497881 10.4022 0.448338 10.0182 0.771252L5.86461 4.26382L2.03491 0.484509C1.67778 0.132076 1.10257 0.135885 0.750134 0.493017Z"
+              :fill="showEmployees ? '#7E7E80' : '#4C4C4D'"
             />
           </svg>
-
-          Очистить
         </button>
-        <EmployeeProfile
-          v-for="contributor in contributorsInfo"
-          :key="contributor.uid"
-          class="mr-3"
-          :photo="contributor.photo"
-          :name="contributor.name"
-        />
+        <div
+          v-if="showEmployees"
+          class="flex items-center mt-2.5 pb-2 flex-wrap gap-y-[10px]"
+        >
+          <button
+            v-if="canEdit"
+            class="flex items-center h-[30px] mr-3 px-[10px] py-[5px] bc-none border-none text-xs text-[#4C4C4D]"
+            @click="clearContributors"
+          >
+            <svg
+              class="mr-1.5"
+              width="20"
+              height="20"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.37364 15.8768H17.7486V17.1268H3.37364V15.8768ZM16.1111 5.57051L11.1549 0.620514C11.0388 0.504294 10.9009 0.412095 10.7492 0.34919C10.5974 0.286284 10.4348 0.253906 10.2705 0.253906C10.1062 0.253906 9.94359 0.286284 9.79184 0.34919C9.64009 0.412095 9.50223 0.504294 9.38614 0.620514L0.636139 9.37051C0.519919 9.48661 0.42772 9.62447 0.364815 9.77621C0.30191 9.92796 0.269531 10.0906 0.269531 10.2549C0.269531 10.4192 0.30191 10.5818 0.364815 10.7336C0.42772 10.8853 0.519919 11.0232 0.636139 11.1393L3.45489 14.0018H9.44864L16.1111 7.33926C16.2274 7.22317 16.3196 7.08531 16.3825 6.93357C16.4454 6.78182 16.4777 6.61916 16.4777 6.45489C16.4777 6.29062 16.4454 6.12796 16.3825 5.97621C16.3196 5.82447 16.2274 5.68661 16.1111 5.57051ZM8.92989 12.7518H3.99864L1.49864 10.2518L5.44239 6.30801L10.3986 11.258L8.92989 12.7518ZM11.2799 10.4018L6.32989 5.44551L10.2486 1.50176L15.2486 6.45801L11.2799 10.4018Z"
+                fill="#4C4C4D"
+              />
+            </svg>
+
+            Очистить
+          </button>
+          <EmployeeProfile
+            v-for="contributor in contributorsInfo"
+            :key="contributor.uid"
+            class="mr-3"
+            :photo="contributor.photo"
+            :name="contributor.name"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -185,6 +188,7 @@ import * as REGLAMENTS from '@/store/actions/reglaments.js'
 import ReglamentTestLimit from '@/components/Reglaments/ReglamentTestLimit.vue'
 import ReglamentEditLimit from '@/components/Reglaments/ReglamentEditLimit.vue'
 import ReglamentSmallButton from '@/components/Reglaments/ReglamentSmallButton.vue'
+import ReglamentContentSkeleton from '@/components/Reglaments/ReglamentContentSkeleton.vue'
 
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
 import EmployeeProfile from '../Employees/EmployeeProfile.vue'
@@ -199,6 +203,7 @@ export default {
     ReglamentSmallButton,
     ReglamentEditLimit,
     ReglamentTestLimit,
+    ReglamentContentSkeleton,
     EmployeeProfile,
     NavBar
   },
@@ -213,7 +218,8 @@ export default {
       showCheckMark: false,
       firstInvalidQuestionUid: null,
       showEmployees: false,
-      reglamentContent: ''
+      reglamentContent: '',
+      reglamentSuccess: false
     }
   },
   computed: {
@@ -338,6 +344,7 @@ export default {
     this.$store.dispatch(REGLAMENTS.REGLAMENT_CONTENT_REQUEST, this.currReglament?.uid).then((res) => {
       console.log(res)
       this.reglamentContent = res.data[0].content
+      this.reglamentSuccess = true
     })
     this.$store.dispatch(REGLAMENTS.REGLAMENT_REQUEST, this.currReglament?.uid)
     this.$store.dispatch(REGLAMENTS.GET_USERS_REGLAMENT_ANSWERS, this.currReglament?.uid)
