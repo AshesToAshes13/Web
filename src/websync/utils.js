fa57cf59155d68ec6433699431db7a8893a92125
Maslov Dmitry <63534992+devmaslove@users.noreply.github.com>
2022-10-14 14:11:56 +0300

    fix Не верно фильтруются задачи в сегодня

--- src/websync/utils.js
+++ src/websync/utils.js
@@ -1,13 +1,26 @@
-import store from '@/store/index.js'
-import router from '@/router/index.js'
-import { computed } from 'vue'
 import { TASK_STATUS } from '@/constants'
+import router from '@/router/index.js'
+import store from '@/store/index.js'
+import { computed } from 'vue'
 
 const user = computed(() => store.state.user.user)
 const settings = computed(() => store.state.navigator.navigator.settings)
 
 function isTaskIsSharedForMe (task) {
-  return (task.emails.includes(user.value.current_user_email) && (task.type === 1 || task.type === 2))
+  return (
+    task.emails.includes(user.value.current_user_email) &&
+    (task.type === 1 || task.type === 2)
+  )
+}
+
+function isTaskCompleted (task) {
+  return (
+    task.status === TASK_STATUS.TASK_COMPLETED ||
+    task.status === TASK_STATUS.TASK_CANCELLED ||
+    (task.type === 3 &&
+      (task.status === TASK_STATUS.TASK_READY ||
+        task.status === TASK_STATUS.TASK_REJECTED))
+  )
 }
 
 export function shouldAddTaskIntoList (task) {
@@ -23,80 +36,68 @@ export function shouldAddTaskIntoList (task) {
 
 const TaskListChecker = {
   shouldAddCompletedTask (task) {
-    const isTaskCompleted = (task.status === TASK_STATUS.TASK_COMPLETED || task.status === TASK_STATUS.TASK_CANCELLED) ||
-        (task.type === 3 && (task.status === TASK_STATUS.TASK_READY || task.status === TASK_STATUS.TASK_REJECTED))
-    if (isTaskCompleted && !settings.value.show_completed_tasks) {
+    if (isTaskCompleted(task) && !settings.value.show_completed_tasks) {
       return false
     }
     return true
   },
   ifTaskHasParent (task) {
-    if (task.uid_parent && task.uid_parent !== '00000000-0000-0000-0000-000000000000') {
-      return true
-    }
-    return false
+    return (
+      task.uid_parent &&
+      task.uid_parent !== '00000000-0000-0000-0000-000000000000'
+    )
   },
   task (task) {
-    if (router.currentRoute.value.params.id === task.uid) {
-      return true
-    }
-    return false
+    return router.currentRoute.value.params.id === task.uid
   },
   search (task) {
-    const searchingText = task.name + task.comment
-    if (searchingText.toLowerCase().includes(router.currentRoute.value.query.q.toLowerCase())) {
-      return true
-    }
-    return false
+    const q = router.currentRoute.value.query.q.toLowerCase()
+    const searchingText = (task.name + task.comment).toLowerCase()
+    return searchingText.includes(q)
   },
   tasksToday (task) {
-    const selectedDate = router.currentRoute.value.name === 'tasksToday' ? new Date() : new Date(router.currentRoute.value.params.date)
+    const selectedDate =
+      router.currentRoute.value.name === 'tasksToday'
+        ? new Date()
+        : new Date(router.currentRoute.value.params.date)
     selectedDate.setHours(0, 0, 0, 0)
-    const isTaskCompleted = (task.status === TASK_STATUS.TASK_COMPLETED || task.status === TASK_STATUS.TASK_CANCELLED) ||
-        (task.type === 3 && (task.status === TASK_STATUS.TASK_READY || task.status === TASK_STATUS.TASK_REJECTED))
+    const currentDate = new Date()
+    currentDate.setHours(0, 0, 0, 0)
+    const isCompleted = isTaskCompleted(task)
+    const isCheckExpired = !settings.value.nav_show_overdue
 
-    if (
-      task.date_begin &&
-      task.date_begin !== '0001-01-01T00:00:00'
-    ) {
+    if (task.date_begin && task.date_begin !== '0001-01-01T00:00:00') {
+      const taskDateBegin = new Date(task.date_begin)
+      taskDateBegin.setHours(0, 0, 0, 0)
       const taskDateEnd = new Date(task.date_end)
       taskDateEnd.setHours(0, 0, 0, 0)
-      const currentDate = new Date()
-      currentDate.setHours(0, 0, 0, 0)
+      const isInPeriod =
+        taskDateEnd >= selectedDate && taskDateBegin <= selectedDate
+      const isExpired = !isCompleted && taskDateEnd < selectedDate
 
-      if (
-        task.type !== 2 &&
-      selectedDate.toLocaleString().split(',')[0] === new Date(task.date_begin).toLocaleString().split(',')[0]
-      ) {
+      // кроме мной порученных задач (их только просроченными отображаем)
+      if (task.type !== 2 && isInPeriod) return true
+
+      if (isCheckExpired && selectedDate <= currentDate && isExpired) {
         return true
-      } else if (
-        selectedDate <= currentDate
-      ) {
-        if (!isTaskCompleted && taskDateEnd < selectedDate) {
-          return false
-        }
       }
     } else if (
       task.customer_date_begin &&
       task.customer_date_begin !== '0001-01-01T00:00:00' &&
       (isTaskIsSharedForMe(task) || task.type === 3)
     ) {
+      const taskDateBegin = new Date(task.customer_date_begin)
+      taskDateBegin.setHours(0, 0, 0, 0)
       const taskDateEnd = new Date(task.customer_date_end)
       taskDateEnd.setHours(0, 0, 0, 0)
-      const currentDate = new Date()
-      currentDate.setHours(0, 0, 0, 0)
+      const isInPeriod =
+        taskDateEnd >= selectedDate && taskDateBegin <= selectedDate
+      const isExpired = !isCompleted && taskDateEnd < selectedDate
 
-      if (
-        task.type !== 2 &&
-        selectedDate.toLocaleString().split(',')[0] === new Date(task.customer_date_begin).toLocaleString().split(',')[0]
-      ) {
+      if (isInPeriod) return true
+
+      if (isCheckExpired && selectedDate <= currentDate && isExpired) {
         return true
-      } else if (
-        selectedDate <= currentDate
-      ) {
-        if (!isTaskCompleted && taskDateEnd < selectedDate) {
-          return true
-        }
       }
     }
     return false
@@ -140,7 +141,6 @@ const TaskListChecker = {
     return false
   },
   tasksOverdue (task) {
-    console.log(task)
     if (
       task.email_performer &&
       task.email_performer === user.value.current_user_email &&
@@ -161,7 +161,8 @@ const TaskListChecker = {
   },
   tasksInWork (task) {
     if (
-      (task.uid_customer === user.value.current_user_uid || task.uid_performer === user.value.current_user_uid) &&
+      (task.uid_customer === user.value.current_user_uid ||
+        task.uid_performer === user.value.current_user_uid) &&
       task.status === TASK_STATUS.TASK_IN_WORK
     ) {
       return true
