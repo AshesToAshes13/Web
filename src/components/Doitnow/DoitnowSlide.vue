79029e5f15114302058dd1ac34352edd980b2cc1
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2022-10-14 16:36:19 +0300

    вынес слайд doitnowstart в отдельный компонент

--- src/components/Doitnow/DoitnowSlide.vue
+++ src/components/Doitnow/DoitnowSlide.vue
@@ -35,44 +35,14 @@
       @close-window="changeAvatar = false"
       @nextTask="nextTask"
     />
-    <!-- doitnowstart -->
-    <div class="flex justify-center h-[87vh] pt-[102px]">
-      <div
+    <!-- Временные паддинги (по дизайну такие) -->
+    <div class="flex justify-center pt-[102px] pb-[185px]">
+      <DoitnowSlideDoitnowStart
         v-if="name === 'doitnowstart'"
-        class="flex flex-col items-center text-center"
-      >
-        <SlideBodyTitle title="Вы в Очереди!" />
-        <div class="flex flex-col items-center gap-[45px]">
-          <div class="max-w-[600px]">
-            <article class="font-normal text-[18px] text-center leading-[29px] w-full pl-0">
-              <p class=" text-[#4C4C4D]">
-                Она выдает вам задачи, заявки и важные уведомления по очереди, а Вы работаете с ними сфокусировано и не отвлекаетесь
-              </p>
-            </article>
-          </div>
-          <img
-            v-if="showPreviewPicture"
-            src="@/assets/images/slides/addEmployes.png"
-            class="w-[560px] h-[315px] cursor-pointer"
-            @click="playVideo"
-          >
-          <iframe
-            v-if="!showPreviewPicture"
-            width="560"
-            height="315"
-            :src="`https://www.youtube.com/embed/Jx-TBirC_Cc?${!showPreviewPicture ? 'autoplay=1' : ''}`"
-            title="YouTube video player"
-            allow="accelerometer;clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-            class="border-[3px] rounded-xl border-[#2E2E2E]"
-          />
-
-          <SlideBodyButton
-            text="Начать"
-            @click="nextTask"
-          />
-        </div>
-      </div>
+        :show-preview-picture="showPreviewPicture"
+        @nextTaskEmit="nextTask"
+        @playVideoEmit="playVideo"
+      />
       <!-- addEmployees -->
       <div
         v-if="name === 'addEmployees'"
@@ -222,6 +192,7 @@ import DoitnowSlidesModalBoxAddEmployee from './DoitnowSlides/DoitnowSlidesModal
 import DoitnowSlidesModalBoxEmployeeLimit from './DoitnowSlides/DoitnowSlidesModalBoxEmployeeLimit.vue'
 import DoitnowSlidesModalBoxOtherOrg from './DoitnowSlides/DoitnowSlidesModalBoxOtherOrg.vue'
 import DoitnowSlidesAlreadyExist from './DoitnowSlides/DoitnowSlidesAlreadyExist.vue'
+import DoitnowSlideDoitnowStart from './DoitnowSlides/DoitnowSlideDoitnowStart.vue'
 
 export default {
   components: {
@@ -234,7 +205,8 @@ export default {
     DoitnowSlidesModalBoxAddEmployee,
     DoitnowSlidesModalBoxEmployeeLimit,
     DoitnowSlidesModalBoxOtherOrg,
-    DoitnowSlidesAlreadyExist
+    DoitnowSlidesAlreadyExist,
+    DoitnowSlideDoitnowStart
   },
   props: {
     name: {
