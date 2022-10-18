9d09fa051f9b284a5bced9174f89070294072678
Alexander Buzmakov <boozy044@gmail.com>
2022-10-18 12:16:17 +0300

    Поправил баг с popper в карточках и задачах

--- src/components/properties/CardProperties.vue
+++ src/components/properties/CardProperties.vue
@@ -26,7 +26,7 @@
   />
   <div
     v-if="selectedCard"
-    class="break-words relative z-1"
+    class="break-words z-1"
   >
     <div class="flex items-center justify-between mb-[10px]">
       <CardOptions
@@ -43,7 +43,7 @@
     </div>
     <div
       id="generalscroll"
-      class="column-resize relative overflow-hidden"
+      class="column-resize overflow-hidden"
     >
       <CardCover
         :cover-color="
