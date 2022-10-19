fc41433818f97414a8071d081766e6d88dd5821c
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-19 18:33:03 +0300

    Сделал стиль под видео в слайдах, чтобы его размер не провоцировал скролл на ноутбуках

--- src/components/Doitnow/DoitnowSlides/DoitnowSlideDoitnowStart.vue
+++ src/components/Doitnow/DoitnowSlides/DoitnowSlideDoitnowStart.vue
@@ -3,7 +3,7 @@
     class="flex flex-col items-center text-center"
   >
     <SlideBodyTitle title="Вы в Очереди!" />
-    <div class="flex flex-col items-center md:gap-[20px] xl:gap-[35px] 2xl:gap-[45px]">
+    <div class="flex flex-col items-center md:gap-[10px] xl:gap-[20px] 2xl:gap-[35px]">
       <div class="max-w-[600px]">
         <article class="font-normal md:text-[16px] lg:text-[18px] text-center leading-[29px] w-full pl-0">
           <p class=" text-[#4C4C4D]">
@@ -14,7 +14,7 @@
       <img
         v-if="showPreviewPicture"
         src="@/assets/images/slides/addEmployes.png"
-        class="cursor-pointer 2xl:w-[560px] 2xl:h-[315px] xl:w-[415px] xl:h-[240px]"
+        class="cursor-pointer doitnow-frame"
         @click="playVideo"
       >
       <iframe
@@ -25,7 +25,7 @@
         title="YouTube video player"
         allow="accelerometer;clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
-        class="border-[3px] rounded-xl border-[#2E2E2E] 2xl:w-[560px] 2xl:h-[315px] xl:w-[415px] xl:h-[240px]"
+        class="border-[3px] rounded-xl border-[#2E2E2E] doitnow-frame"
       />
 
       <SlideBodyButton
