cf60dca8181c590d893de3aa55d818778ac583a6
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-20 15:53:39 +0300

    Правки по верстке интеграции мегафон

--- src/components/ButtonCopy.vue
+++ src/components/ButtonCopy.vue
@@ -2,7 +2,7 @@
   <div
     class="flex align-middle grow-5 text-[13px] bg-[#F4F5F7] rounded-[6px] w-full"
   >
-    <div class="flex-1 px-[11px] py-[13px]">
+    <div class="flex-1 px-[11px] py-[13px] text-[#424242]">
       <span>{{ text }}</span>
     </div>
     <button
