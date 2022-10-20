28e42ee6cf295b0372d954227c9f4a69659ba300
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2022-10-20 17:46:27 +0300

    хелпер для account роута
    сделано чтобы нельзя было перейти на голый роут 'account', который пустой и переходить только на вкладки по типу "Аккаунт"

--- src/router/account.js
+++ src/router/account.js
@@ -1,30 +1,23 @@
-import { shouldRedirectToLogin } from './utils'
+import { prefixRoutes } from './utils'
 import Home from '@/views/Home'
 
 export default [
-  {
-    meta: {
-      layout: Home
-    },
-    path: '/account',
-    beforeEnter: shouldRedirectToLogin,
-    children: [
-      {
-        path: 'myaccount',
-        name: 'myaccount',
-        component: () => import('@/components/Settings/Account.vue'),
-        meta: {
-          title: 'Аккаунт'
-        }
-      },
-      {
-        path: 'karma',
-        name: 'karma',
-        component: () => import('@/components/Settings/AccKarma.vue'),
-        meta: {
-          title: 'Карма'
-        }
+  ...prefixRoutes('/account/', Home, [
+    {
+      path: 'myaccount',
+      name: 'myaccount',
+      component: () => import('@/components/Settings/Account.vue'),
+      meta: {
+        title: 'Аккаунт'
       }
-    ]
-  }
+    },
+    {
+      path: 'karma',
+      name: 'karma',
+      component: () => import('@/components/Settings/AccKarma.vue'),
+      meta: {
+        title: 'Карма'
+      }
+    }
+  ])
 ]
