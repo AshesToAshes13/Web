336d0aff5a855d04fbb2a61bd11c890b29fe260b
Maslov Dmitry <63534992+devmaslove@users.noreply.github.com>
2022-10-21 17:06:48 +0300

    Рефакторинг
    Переверстал свойства задач на флексы

--- src/components/properties/TaskProperties.vue
+++ src/components/properties/TaskProperties.vue
@@ -28,15 +28,18 @@
     @cancel="showFileSizeLimit = false"
   />
   <div
-    class="break-words z-1"
+    class="h-[calc(100vh-20px)] flex flex-col"
     @mousedown="selectedFalse"
   >
-    <div class="flex items-center justify-end mb-[10px]">
+    <div class="flex-none flex items-center justify-end mb-[10px]">
       <PropsButtonClose
         @click="closeProperties"
       />
     </div>
-    <div id="task-prop-content">
+    <div
+      id="task-prop-content"
+      class="flex-1 overflow-x-hidden w-full scroll-style"
+    >
       <div class="flex items-center">
         <TaskStatus
           :task="selectedTask"
@@ -202,62 +205,62 @@
         @deleteTaskMsg="deleteTaskMsg"
       />
     </div>
-  </div>
-  <div class="w-full">
-    <img
-      v-if="isloading"
-      class="mt-[8px] h-[40px]"
-      src="/ajaxloader.gif"
-    >
-    <div
-      v-if="currentAnswerMessageUid"
-      class="quote-request border-l-2 border-[#7E7E80] mt-[8px] h-[40px]"
-    >
-      <div class="flex flex-row items-center">
-        <div class="grow w-[calc(100%-20px)]">
+    <div class="flex-none w-full mt-[8px] mb-[20px]">
+      <img
+        v-if="isloading"
+        class="mt-[8px] h-[40px]"
+        src="/ajaxloader.gif"
+      >
+      <div
+        v-if="currentAnswerMessageUid"
+        class="quote-request border-l-2 border-[#7E7E80] mt-[8px] h-[40px]"
+      >
+        <div class="flex flex-row items-center">
+          <div class="grow w-[calc(100%-20px)]">
+            <div
+              class="mx-[4px]"
+            >
+              <p class="text-[11px] leading-[16px] overflow-hidden text-black text-ellipsis whitespace-nowrap">
+                {{ messageQuoteUser }}
+              </p>
+              <p class="text-[12px] leading-[16px] overflow-hidden text-[#9a9fa6] text-ellipsis whitespace-nowrap">
+                {{ messageQuoteString }}
+              </p>
+            </div>
+          </div>
           <div
-            class="mx-[4px]"
+            class="flex-none"
+            @click="onAnswerMessage('')"
           >
-            <p class="text-[11px] leading-[16px] overflow-hidden text-black text-ellipsis whitespace-nowrap">
-              {{ messageQuoteUser }}
-            </p>
-            <p class="text-[12px] leading-[16px] overflow-hidden text-[#9a9fa6] text-ellipsis whitespace-nowrap">
-              {{ messageQuoteString }}
-            </p>
+            <svg
+              class="m-[2px]"
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M4.28481 4.30107C4.67082 3.90608 5.30395 3.8988 5.69893 4.28481L9.25 7.75517L12.8011 4.28481C13.1961 3.8988 13.8292 3.90608 14.2152 4.30107C14.6012 4.69605 14.5939 5.32918 14.1989 5.71519L10.6808 9.15341L14.1989 12.5916C14.5939 12.9776 14.6012 13.6108 14.2152 14.0058C13.8292 14.4007 13.1961 14.408 12.8011 14.022L9.25 10.5516L5.69893 14.022C5.30395 14.408 4.67082 14.4007 4.28481 14.0058C3.8988 13.6108 3.90608 12.9776 4.30107 12.5916L7.81925 9.15341L4.30107 5.71519C3.90608 5.32918 3.8988 4.69605 4.28481 4.30107Z"
+                fill="#999999"
+              />
+            </svg>
           </div>
         </div>
-        <div
-          class="flex-none"
-          @click="onAnswerMessage('')"
-        >
-          <svg
-            class="m-[2px]"
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M4.28481 4.30107C4.67082 3.90608 5.30395 3.8988 5.69893 4.28481L9.25 7.75517L12.8011 4.28481C13.1961 3.8988 13.8292 3.90608 14.2152 4.30107C14.6012 4.69605 14.5939 5.32918 14.1989 5.71519L10.6808 9.15341L14.1989 12.5916C14.5939 12.9776 14.6012 13.6108 14.2152 14.0058C13.8292 14.4007 13.1961 14.408 12.8011 14.022L9.25 10.5516L5.69893 14.022C5.30395 14.408 4.67082 14.4007 4.28481 14.0058C3.8988 13.6108 3.90608 12.9776 4.30107 12.5916L7.81925 9.15341L4.30107 5.71519C3.90608 5.32918 3.8988 4.69605 4.28481 4.30107Z"
-              fill="#999999"
-            />
-          </svg>
-        </div>
       </div>
+      <CardMessageInput
+        v-model="taskMsg"
+        class="mt-[8px]"
+        :can-add-files="!isLicenseExpired"
+        @cantWriteMessages="showFreeModalChat = true"
+        @createCardMessage="sendTaskMsg"
+        @createCardFile="createTaskFile"
+        @onPaste="onPasteEvent"
+        @changeHeight="onChangeChatInputHeight"
+      />
     </div>
-    <CardMessageInput
-      v-model="taskMsg"
-      class="mt-[16px]"
-      :can-add-files="!isLicenseExpired"
-      @cantWriteMessages="showFreeModalChat = true"
-      @createCardMessage="sendTaskMsg"
-      @createCardFile="createTaskFile"
-      @onPaste="onPasteEvent"
-      @changeHeight="onChangeChatInputHeight"
-    />
   </div>
 </template>
 
@@ -1259,15 +1262,5 @@ export default {
   border: 1px solid #aaaaaa;
 }
 
-:root {
-  --task-props-static-height: 126px;
-}
-
-#task-prop-content {
-  height: calc(100vh - var(--task-props-static-height));
-  overflow-y: auto;
-  width: 100%;
-  overflow-x: hidden;
-}
 #task-prop-content::-webkit-scrollbar { width: 0; }
 </style>
