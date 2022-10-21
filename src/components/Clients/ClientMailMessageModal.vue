ce499a32403eb97cd2ac099d44ff62621c58644d
Bukharin Maxim <jojojopa@icloud.com>
2022-10-21 18:59:37 +0300

    фикс сообщений от яндекс интеграции, вместо голого html кода html код теперь визуализируется

--- src/components/Clients/ClientMailMessageModal.vue
+++ src/components/Clients/ClientMailMessageModal.vue
@@ -4,28 +4,14 @@
     width="400"
     @cancel="$emit('closeModal')"
   >
-    <div
-      v-linkified:options="{ className: 'text-blue-600', tagName: 'a' }"
-      class="h-[70vh] w-full overflow-y-scroll scroll-style pr-[20px]"
-    >
-      <contenteditable
-        v-model="insideMessage"
-        tag="div"
-        class="ring-0 outline-none text-[#424242] break-words"
-        style="word-break: break-word"
-        :contenteditable="false"
-        :no-nl="false"
-        :no-html="false"
-      />
-    </div>
+    <div v-html="insideMessage" />
   </ModalBox>
 </template>
 <script>
 import ModalBox from '@/components/modals/ModalBox'
-import contenteditable from 'vue-contenteditable'
 
 export default {
-  components: { ModalBox, contenteditable },
+  components: { ModalBox },
   props: {
     subject: {
       type: String,
