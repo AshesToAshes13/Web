e3f4cafce494bc883c3119f99a700eab207370bd
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-25 17:59:43 +0300

    Поправил видео в интеграции мегафон

--- src/components/Integrations/CoproIntegrationsMegafon.vue
+++ src/components/Integrations/CoproIntegrationsMegafon.vue
@@ -197,7 +197,7 @@
         >
         <iframe
           v-if="!showPreviewPicture"
-          :src="`https://www.youtube.com/embed/JlsbiJv78qg?${!showPreviewPicture ? 'autoplay=1' : ''}`"
+          :src="`https://www.youtube.com/embed/xM9vMQmNx5Q?${!showPreviewPicture ? 'autoplay=1' : ''}`"
           title="YouTube video player"
           allow="accelerometer;clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
