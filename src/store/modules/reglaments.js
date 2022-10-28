4a2fe93c479e73a6b664dda98cc6bff8e205ad7e
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2022-10-28 11:18:28 +0300

    правки по вебсинку ремайндера
    сейчас должно обновляться в очереди

--- src/store/modules/reglaments.js
+++ src/store/modules/reglaments.js
@@ -402,7 +402,8 @@ const mutations = {
   },
   [REGLAMENTS.CHANGE_REGLAMENT]: (state, reglament) => {
     state.reglaments[reglament.uid] = reglament
-    state.reglaments[reglament.uid].reminder = reglament.reminder || null
+    state.reglaments[reglament.uid].reminder = reglament.reminder ?? ''
+    state.reglaments[reglament.uid].department_uid = reglament.department_uid ?? ''
   },
   [REGLAMENTS.RESET_REGLAMENTS_STATE]: (state) => {
     Object.assign(state, getDefaultState())
