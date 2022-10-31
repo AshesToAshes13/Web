c3a5fa5b1336735968ada32d620f719585f02a81
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-31 14:14:08 +0300

    Убрал kebab-case в CardChatSelfFileMessage.vue

--- src/components/CardProperties/CardChatSelfFileMessage.vue
+++ src/components/CardProperties/CardChatSelfFileMessage.vue
@@ -2,8 +2,8 @@
   <div
     class="bg-[#F4F5F7] py-[10px] px-[12px] rounded-t-[12px] rounded-bl-[12px] mb-[5px] float-right relative max-w-[300px] group"
   >
-    <card-chat-deleted-message-content v-if="message.deleted" />
-    <image-preloader
+    <CardChatDeletedMessageContent v-if="message.deleted" />
+    <ImagePreloader
       v-else-if="FileIsImage"
       :file-uid="message.uid"
       :file-size="message.file_size"
@@ -15,7 +15,7 @@
       @onQuoteMessage="setCurrentQuote"
       @onDeleteMessage="deleteFile"
     />
-    <audio-preloader
+    <AudioPreloader
       v-else-if="FileIsAudio"
       :file-uid="message.uid"
       :file-extension="fileExtension"
@@ -25,7 +25,7 @@
       @onQuoteMessage="setCurrentQuote"
       @onDeleteMessage="deleteFile"
     />
-    <doc-preloader
+    <DocPreloader
       v-else-if="FileIsDoc"
       :file-uid="message.uid"
       :file-name="message.file_name"
@@ -37,7 +37,7 @@
       @onQuoteMessage="setCurrentQuote"
       @onDeleteMessage="deleteFile"
     />
-    <movie-preloader
+    <MoviePreloader
       v-else-if="FileIsMovie"
       :file-uid="message.uid"
       :file-extension="fileExtension"
@@ -49,7 +49,7 @@
       @onQuoteMessage="setCurrentQuote"
       @onDeleteMessage="deleteFile"
     />
-    <file-preloader
+    <FilePreloader
       v-else
       :file-uid="message.uid"
       :file-extension="fileExtension"
