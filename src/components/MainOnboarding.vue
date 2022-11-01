8564dbb7c93c1a039e4c39e84853040e661c703c
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2022-11-01 17:51:20 +0300

    вставил новый видос, стили
    поправил на странице добро пожаловать( убрал дублированные, итемы по центру

--- src/components/MainOnboarding.vue
+++ src/components/MainOnboarding.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="flex flex-col max-w-xl mx-auto mt-[100px]"
+    class="flex flex-col max-w-xl mx-auto mt-[100px] items-center"
   >
     <div class="w-[600px] font-roboto text-[#424242] mb-10">
-      <p class="font-[700] mb-10 text-left text-[25px] leading-[35px] text-center">
+      <p class="font-[700] mb-10 text-[25px] leading-[35px] text-center">
         Добро пожаловать!
       </p>
       <p class="font-[400] text-[18px] text-center leading-[28px]">
@@ -20,9 +20,10 @@
       v-if="!showPreviewPicture"
       width="560"
       height="315"
-      :src="`https://www.youtube.com/embed/Jx-TBirC_Cc?${!showPreviewPicture ? 'autoplay=1' : ''}`"
+      :src="`https://www.youtube.com/embed/1SCpt6BJz6E?${!showPreviewPicture ? 'autoplay=1' : ''}`"
       title="YouTube video player"
-      allow="accelerometer;clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen
       class="border-[3px] rounded-xl border-[#2E2E2E]"
     />
