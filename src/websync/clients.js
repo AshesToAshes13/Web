28a199d94448c76e2c07ccde83f082d84428ad40
Samarkin Danil <samarkin.danil@yandex.ru>
2022-11-03 12:35:22 +0300

    Вебсинк удаления клиента с карточек, при удалении клиента

--- src/websync/clients.js
+++ src/websync/clients.js
@@ -23,9 +23,22 @@ export function removeClient (obj) {
   if (router.currentRoute.value.params.client_id === obj) {
     router.push('/clients')
   }
-  store.commit(CLIENTS.REMOVE_CLIENT, obj.obj)
+  store.commit(CLIENTS.REMOVE_CLIENT, obj)
 
   helperFuncUpdate()
+
+  if (store.state.cards.cards.length) {
+    // Удаляем клиента с карточек
+    const cards = store.state.cards.cards
+    for (let i = 1; i < cards.length - 2; i++) {
+      cards[i].cards.forEach(card => {
+        if (card.uid_client === obj) {
+          card.uid_client = ''
+          card.client_name = ''
+        }
+      })
+    }
+  }
 }
 
 export function updateClient (obj) {
