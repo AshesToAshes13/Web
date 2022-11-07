1f666735fd47caab669eb1e66d4662272086235a
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2022-11-07 12:45:41 +0300

    вынес "Имя" в аккаунте в отдельный компонент

--- src/components/Settings/Account.vue
+++ src/components/Settings/Account.vue
@@ -144,21 +144,10 @@
           :get-date-expired="getDateExpired"
         />
         <div class="mt-6">
-          <p class="text-base font-medium mb-2 text-[#4C4C4D]">
-            Имя
-          </p>
-          <form class="mb-2">
-            <div class="text-sm landing-4 font-normal text-[#606061]">
-              {{ user?.current_user_name ?? '' }}
-            </div>
-            <button
-              type="button"
-              class="mt-2 text-[14px] landing-[13px] text-[#007BE5]"
-              @click="showEditname = true"
-            >
-              Изменить имя
-            </button>
-          </form>
+          <AccountName
+            :user-name="user?.current_user_name"
+            @showEditnameModalEmit="showEditnameModal"
+          />
           <div class="mt-6">
             <p class="text-base font-medium mb-2 text-[#4C4C4D]">
               Телефон
@@ -232,6 +221,7 @@ import UploadAvatar from '@/components/UploadAvatar'
 import * as TASK from '@/store/actions/tasks.js'
 import * as DOITNOW from '@/store/actions/doitnow.js'
 import AccountTarif from '../Account/AccountTarif.vue'
+import AccountName from '../Account/AccountName.vue'
 
 export default {
   components: {
@@ -240,7 +230,8 @@ export default {
     UsernameRename,
     PhoneModalBoxRename,
     NavBar,
-    AccountTarif
+    AccountTarif,
+    AccountName
   },
   emits: ['AccLogout'],
   data () {
@@ -325,6 +316,9 @@ export default {
         name: 'addAvatar',
         visible: true
       })
+    },
+    showEditnameModal () {
+      this.showEditname = true
     },
     changeUserPhoto (event) {
       const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg']
