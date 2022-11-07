820f98378f5420f187ec4d2c9599107503b10962
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-11-07 18:21:05 +0300

    Рефакторинг BoardCard.vue, разбил на компоненты

---
+++ src/components/Board/card/BoardCardEmployee.vue
@@ -0,0 +1,37 @@
+<template>
+  <img
+    v-if="getEmpPhotoByEmail(cardEmployee)"
+    class="w-[22px] h-[22px] rounded-[5px] border border-black/10"
+    :src="getEmpPhotoByEmail(cardEmployee)"
+    alt="Avatar"
+  >
+  <span
+    class="ml-1 text-[12px] leading-[14px] text-[#777777] font-['Roboto'] truncate"
+  >
+    {{ getEmpNameByEmail(cardEmployee) }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    cardEmployee: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    employeesByEmail () {
+      return this.$store.state.employees.employeesByEmail
+    }
+  },
+  methods: {
+    getEmpPhotoByEmail (userEmail) {
+      return this.employeesByEmail[userEmail.toLowerCase()]?.fotolink ?? ''
+    },
+    getEmpNameByEmail (userEmail) {
+      return this.employeesByEmail[userEmail.toLowerCase()]?.name || userEmail
+    }
+  }
+}
+</script>
